656a1f587cd3f2c2f720324e3a4ce2b55616fdca
allllleeeex <agepp27@gmail.com>
2024-10-28 15:14:57 +0000

    hh

---
+++ GridPath.java
@@ -0,0 +1,18 @@
+
+public class GridPath {
+    private int[][] grid;
+
+    public gridPath(int[][] values)
+{  
+    grid = values;
+    
+}
+
+public Location getNextLoc (int row, int col) {
+    return null; }
+
+    public int sumPath (int row, int col) {
+    return 0; }
+
+
+
